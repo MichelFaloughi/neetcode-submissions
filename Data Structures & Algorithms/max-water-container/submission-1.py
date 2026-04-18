94cbd4e8e3c9e40ca73202c30b30ee4d5b74b5ff
Michel Faloughi <157075227+MichelFaloughi@users.noreply.github.com>
2026-04-18 17:54:21 -0400

    Add: max-water-container - submission-1

---
+++ Data Structures & Algorithms/max-water-container/submission-1.py
@@ -0,0 +1,29 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        
+        # assuming l < r
+        # given two indices l, r
+        # take h = min(l,r), w = r - l, wtr = h * w
+        # 
+        
+        maxWater = 0
+        l, r = 0, len(heights) - 1
+
+        while l < r:
+            currWater = min(heights[l], heights[r]) * (r - l) # height * width
+            maxWater = max(maxWater, currWater)
+
+            if heights[l] < heights[r]:
+                l += 1
+            else:
+                r -= 1
+            
+        return maxWater
+
+
+# learnings:
+# - Moving the taller line never helps because it keeps the height the same but reduces the width.
+# - By always moving the shorter side, we explore all meaningful possibilities.
+
+# We can optimize in two dimensions, width and height. We start at the biggest possible width, it won't get any better
+# so just make sure you keep the height as high as possible, hence why we move the smaller 'tower'
