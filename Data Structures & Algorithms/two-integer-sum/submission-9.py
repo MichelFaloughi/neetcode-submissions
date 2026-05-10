cd0b5e6536d9c663155eccf49919576f9537a4f3
Michel Faloughi <157075227+MichelFaloughi@users.noreply.github.com>
2026-05-10 09:09:37 -0400

    Add: two-integer-sum - submission-9

---
+++ Data Structures & Algorithms/two-integer-sum/submission-9.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        # [ 3, 4, 5, 6 ], target = 7
+
+        # nums.sort()
+        comps_to_idx = {} # int (comp) -> int (idx)
+
+        for i, num in enumerate(nums):
+            comp = target - num
+            
+            if comp in comps_to_idx:
+                return [comps_to_idx[comp], i ]
+            
+            comps_to_idx[num] = i 
+        
+        return [-1, -1]
+        
+
+
+
